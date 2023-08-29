3073fb6c5f79b8b37e2dac9fb7b39f0e6f8f04a8
Isaac Shaha <isaac.shaha64@gmail.com>
2023-08-29 09:48:35 -0600

    Finalised dev environment with system GHC from pinned nixpkgs

--- shells/haskell-textbook/shell.nix
+++ shells/haskell-textbook/shell.nix
@@ -1,7 +1,13 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell {
+{ basePkgs ? import <nixpkgs> { } }:
+let
+  pinnedPkgsSource = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/74d8ebc574986457eb4b8c2d0d8f9c37b72820e4.tar.gz";
+  };
+  pinnedPkgs = import pinnedPkgsSource { };
+in
+pinnedPkgs.mkShell {
   buildInputs = [
-    pkgs.stack
+    pinnedPkgs.haskell.compiler.ghc865
+    pinnedPkgs.stack
   ];
 }
